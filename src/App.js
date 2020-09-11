b0c2160d953370a78669bd7bac3fe8479d3d0dc0
root <root@DESKTOP-GVT4GDD.localdomain>
2020-09-11 13:51:17 +0100

    changed text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React.
+          Learn React, pretty please.
         </a>
       </header>
     </div>
